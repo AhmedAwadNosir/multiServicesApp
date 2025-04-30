5cb2ce571f129e797718f808fb7f9428501359ac
AhmedAwadNosir <ahmedalprens98@gmail.com>
2025-04-30 16:06:18 +0300

    apply method of change language to any chosen language

--- lib/l10n/localization_provider.dart
+++ lib/l10n/localization_provider.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:multiservices_app/core/utils/app_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 
 class LocalizationProvider with ChangeNotifier {
   Locale _locale;
 
   LocalizationProvider(String lang) : _locale = Locale(lang);
   Locale get locale => _locale;
-
   void toggleLanguage() async {
     final prefs = await SharedPreferences.getInstance();
     if (_locale == Locale('ar')) {
@@ -17,6 +17,15 @@ class LocalizationProvider with ChangeNotifier {
       _locale = Locale('ar');
       prefs.setString(AppConstants.lang, 'ar');
     }
+    notifyListeners();
+  }
+
+  // this is used if i have more than two languages deflut is en when i chang lang
+  // lang defult is new lang
+  void toggleLanguageSpicificLanguage({required String lang}) async {
+    final prefs = await SharedPreferences.getInstance();
+    _locale = Locale(lang);
+    prefs.setString(AppConstants.lang, lang);
     notifyListeners();
   }
 }
